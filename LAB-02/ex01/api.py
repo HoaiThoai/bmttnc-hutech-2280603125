a2f0bd5937fbc4b556e9c0d7b122b924b135796c
HoaiThoai <hoaithoai35@gmail.com>
2025-03-10 14:11:27 +0700

    [add] lab-2 web caesar

--- LAB-02/ex01/api.py
+++ LAB-02/ex01/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher  
 from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailFenceCipher
 
 app = Flask(__name__)
 
@@ -62,5 +63,35 @@ def vigenere_decrypt():
     key = data['key']
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
+
+
+# RAILFENCE CIPHER ALGORITHM
+railfence_cipher = RailFenceCipher()
+
+@app.route('/api/railfence/encrypt', methods=['POST'])
+def encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/railfence/decrypt', methods=['POST'])
+def decrypt():
+    try:
+        data = request.json
+        print("Received data:", data)  # Debug print to check the content of data
+        if not data or 'cipher_text' not in data or 'key' not in data:
+            return jsonify({"error": "Thiếu dữ liệu đầu vào"}), 400
+
+        cipher_text = data['cipher_text']
+        key = int(data['key'])
+        decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+        return jsonify({'decrypted_text': decrypted_text})
+    except ValueError:
+        return jsonify({"error": "Key phải là số nguyên"}), 400
+    except Exception as e:
+        return jsonify({"error": f"Lỗi hệ thống: {str(e)}"}), 500
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)  
